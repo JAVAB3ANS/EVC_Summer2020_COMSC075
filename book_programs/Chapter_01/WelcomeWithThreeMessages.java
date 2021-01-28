d46cb607bb61597592a2fdad2522152778b3f0d9
Jason Vu <jvu@scu.edu>
2021-01-27 17:17:34 -0800

    UPDATED Folder names

---
+++ book_programs/Chapter_01/WelcomeWithThreeMessages.java
@@ -0,0 +1,9 @@
+package book_programs.Chapter_1;
+
+public class WelcomeWithThreeMessages {   
+    public static void main(String[] args) {     
+        System.out.println("Programming is fun!");    
+        System.out.println("Fundamentals First");    
+        System.out.println("Problem Driven");  
+    }
+}
--- book_programs/Chapter_1/WelcomeWithThreeMessages.java
+++
@@ -1,9 +0,0 @@
-package book_programs.Chapter_1;
-
-public class WelcomeWithThreeMessages {   
-    public static void main(String[] args) {     
-        System.out.println("Programming is fun!");    
-        System.out.println("Fundamentals First");    
-        System.out.println("Problem Driven");  
-    }
-}
